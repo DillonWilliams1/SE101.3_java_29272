9a5a597627affcf8c8129f47522c606cd402745f
DillonWilliams1 <136365918+DillonWilliams1@users.noreply.github.com>
2023-06-20 00:36:51 +0530

    App.java

--- lab05-2023.06.12/App.java
+++ lab05-2023.06.12/App.java
@@ -1,5 +1,7 @@
+package com.mycompany.practical5;
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+     public static void main(String[] args)
+    {
+      
     }
 }
