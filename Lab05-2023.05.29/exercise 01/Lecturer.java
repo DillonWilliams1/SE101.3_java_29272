9517988178593ab8b71f62c2adc8071b77f3b1e7
DillonWilliams1 <136365918+DillonWilliams1@users.noreply.github.com>
2023-07-31 21:53:45 +0530

    Add files via upload

---
+++ Lab05-2023.05.29/exercise 01/Lecturer.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Lecturer extends Person {
+    
+    private String programme;
+
+   
+    public Lecturer(String name, int id, String programme) {
+       
+        super(name, id);
+        this.programme = programme;
+    }
+
+   
+    public String getProgramme() {
+        return programme;
+    }
+
+    public void setProgramme(String programme) {
+        this.programme = programme;
+    }
+}
